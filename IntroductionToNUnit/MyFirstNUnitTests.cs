bc817bd7db99674406f7c753afb150dad2c73e29
wilsongumba <wilsongumba@gmail.com>
2023-08-01 02:21:45 +0300

    chapter 3 - first tests

---
+++ IntroductionToNUnit/MyFirstNUnitTests.cs
@@ -0,0 +1,29 @@
+﻿namespace IntroductionToNUnit;
+
+public class MyFirstNUnitTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void PassingTest()
+    {
+        Console.WriteLine("Check the console");
+        Assert.True(true);
+        Console.WriteLine("Past the breakpoint");
+    }
+
+    [Test]
+    public void FailingTest()
+    {
+        Assert.True(false);
+    }
+}
